9906187c18d5d2a0aaad25cd8c9cff057183471c
Ruiz, Luis Diego <oracleruiz@gmail.com>
2018-05-10 15:22:20 -0600

    Removing test component reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
