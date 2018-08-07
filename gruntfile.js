b051bb92cbd941cfb20b09375b6a54732ae67081
Lloyd Stumpel <lloyd.stumpel@poort80.nl>
2018-08-07 09:50:06 +0200

    added a basic scss 'skeleton' library

---
+++ gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt, {
+        config: './package.json',
+        scope: 'devDependencies'
+    });
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('./package.json'),
+        config: {
+            webroot: './src',
+            scss: './src/scss',
+            css: '<%= config.webroot %>/assets/css'
+        },
+
+        watch: {
+            options: {
+                spawn: false
+            },
+            base: {
+                files: '<%= config.scss %>/**/*.scss',
+                tasks: ['sass:dev']
+            },
+        },
+
+        sass: {
+            dev: {
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMap: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.scss %>',
+                    src: ['*.scss'],
+                    dest: '<%= config.css %>',
+                    ext: '.css',
+                }]
+            },
+            dist: {
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMap: false
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= config.scss %>',
+                    src: ['*.scss'],
+                    dest: '<%= config.css %>',
+                    ext: '.css',
+                }]
+            },
+        }
+    });
+
+    grunt.registerTask('build-sass', 'Compile Scss to CSS', [
+        'sass:dist'
+    ]);
+};
